a485b1256ecfc854a53f2c0ef8b71d906ff5bc6d
adrian <suryawanadriansyah4@gmail.com>
2023-04-04 17:01:55 +0700

    modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,9 +119,9 @@
                 <aside class="main-sidebar main-sidebar-custom sidebar-light-primary elevation-4">
                     <!-- Brand Logo -->
                     <a href="../index3.html" class="brand-link">
-                        <img src="{{ url('adminlte/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                        <img src="{{ url('adminlte/img/neper.png') }}" alt="AdminLTE Logo"
                             class="brand-image img-circle elevation-3" style="opacity: .8">
-                        <span class="brand-text font-weight-light">AdminLTE 3</span>
+                        <span class="brand-text font-weight-light">Dispensasi</span>
                     </a>
 
                     <!-- Sidebar -->
@@ -131,7 +131,7 @@
                             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                                 data-accordion="false">
                                 <!-- Add icons to the links using the .nav-icon class
-                   with font-awesome or any other icon font library -->
+                    with font-awesome or any other icon font library -->
                                 <li class="nav-item">
                                     <a href="{{ route('dashboard') }}" class="nav-link active">
                                         <i class="nav-icon fas fa-tachometer-alt"></i>
