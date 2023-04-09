dea66903d46ba2dc3b0697cb70fc1768648c8fe8
adrian <suryawanadriansyah4@gmail.com>
2023-04-09 16:42:06 +0700

    dispensasi controller

--- app/Http/Controllers/DispensasiController.php
+++ app/Http/Controllers/DispensasiController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kelas;
+use App\Models\Dispensasi;
+use App\Models\JamPelajaran;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DispensasiController extends Controller
 {
@@ -11,7 +15,16 @@ class DispensasiController extends Controller
      */
     public function index()
     {
-        return view('pages.dispensasi.index');
+        $dispensasi = Dispensasi::join('kelas', 'dispensasi.id_kelas', '=', 'kelas.id')
+            ->join('jurusan', 'dispensasi.id_jurusan', '=', 'jurusan.id')
+            ->join('jam_pelajaran', 'dispensasi.jam_mulai', '=', 'jam_pelajaran.id')
+            ->join('jam_pelajaran', 'dispensasi.jam_kembali', '=', 'jam_pelajaran.id')
+            ->get();
+
+
+        // $dispensasi = DB::table('dispensasi')
+
+        return view('pages.dispensasi.index', compact('dispensasi'));
     }
 
     /**
@@ -19,7 +32,10 @@ class DispensasiController extends Controller
      */
     public function create()
     {
-        //
+        $kelas = Kelas::get();
+        $jam = JamPelajaran::get();
+
+        return view('formulir', compact('kelas', 'jam'));
     }
 
     /**
@@ -27,7 +43,22 @@ class DispensasiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Dispensasi::insert([
+            'nama_siswa' => $request->nama_siswa,
+            'kelas' => $request->kelas,
+            'jam_pelajaran' => $request->jam_pelajaran,
+            'jam_mulai' => $request->jam_mulai,
+            'jam_kembali' => $request->jam_kembali,
+            'keperluan' => $request->keperluan,
+            'waktu_input' => $request->waktu_input,
+            'email' => $request->email,
+            'plat_no' => $request->plat_no,
+            'status' => $request->status,
+            'id_kelas' => $request->id_kelas,
+            'id_jurusan' => $request->id_jurusan,
+        ]);
+
+        return redirect('/');
     }
 
     /**
