dadf62ea55cf489c324cd8816e9bade558b23a95
adrian <suryawanadriansyah4@gmail.com>
2023-04-17 00:55:15 +0700

    finishing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -929,63 +929,67 @@
                 </div>
             </div>
         </section>
-        <!-- ======= Services Section ======= -->
-        <!-- <section id="services" class="services ">
-              <div class="container">
+        <div class="container">
 
-                <div class="section-title pt-5" data-aos="fade-up">
-                  <h2>Our Services</h2>
+            <div class="section-title pt-5" data-aos="fade-up">
+                <h2>Our Services</h2>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="icon-box" data-aos="fade-up">
+                        <div class="icon"><i class="bi bi-briefcase" style="color: #ff689b;"></i></div>
+                        <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+                        <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias
+                            excepturi sint occaecati cupiditate non provident</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="icon-box" data-aos="fade-up">
+                        <div class="icon"><i class="bi bi-book" style="color: #e9bf06;"></i></div>
+                        <h4 class="title"><a href="">Dolor Sitema</a></h4>
+                        <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat tarad limino ata</p>
+                    </div>
                 </div>
 
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="icon-box" data-aos="fade-up">
-                      <div class="icon"><i class="bi bi-briefcase" style="color: #ff689b;"></i></div>
-                      <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-                      <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="icon-box" data-aos="fade-up">
-                      <div class="icon"><i class="bi bi-book" style="color: #e9bf06;"></i></div>
-                      <h4 class="title"><a href="">Dolor Sitema</a></h4>
-                      <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-                    </div>
-                  </div>
-
-                  <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
                     <div class="icon-box">
-                      <div class="icon"><i class="bi bi-card-checklist" style="color: #3fcdc7;"></i></div>
-                      <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-                      <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                        <div class="icon"><i class="bi bi-card-checklist" style="color: #3fcdc7;"></i></div>
+                        <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+                        <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            dolore eu fugiat nulla pariatur</p>
                     </div>
-                  </div>
-                  <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+                </div>
+                <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
                     <div class="icon-box">
-                      <div class="icon"><i class="bi bi-binoculars" style="color:#41cf2e;"></i></div>
-                      <h4 class="title"><a href="">Magni Dolores</a></h4>
-                      <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <div class="icon"><i class="bi bi-binoculars" style="color:#41cf2e;"></i></div>
+                        <h4 class="title"><a href="">Magni Dolores</a></h4>
+                        <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                            officia deserunt mollit anim id est laborum</p>
                     </div>
-                  </div>
-
-                  <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
-                    <div class="icon-box">
-                      <div class="icon"><i class="bi bi-globe" style="color: #d6ff22;"></i></div>
-                      <h4 class="title"><a href="">Nemo Enim</a></h4>
-                      <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-                    </div>
-                  </div>
-                  <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
-                    <div class="icon-box">
-                      <div class="icon"><i class="bi bi-clock" style="color: #4680ff;"></i></div>
-                      <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-                      <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-                    </div>
-                  </div>
                 </div>
 
-              </div>
-            </section>End Services Section -->
+                <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><i class="bi bi-globe" style="color: #d6ff22;"></i></div>
+                        <h4 class="title"><a href="">Nemo Enim</a></h4>
+                        <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                            blanditiis praesentium voluptatum deleniti atque</p>
+                    </div>
+                </div>
+                <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><i class="bi bi-clock" style="color: #4680ff;"></i></div>
+                        <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
+                        <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero
+                            tempore, cum soluta nobis est eligendi</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        </section>End Services Section -->
 
     </main><!-- End #main -->
 
