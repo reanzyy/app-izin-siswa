5bb84340b81650ddc4e641a015d39579ddc00040
adrian <suryawanadriansyah4@gmail.com>
2023-05-01 12:50:14 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
-use App\Models\Jurusan;
-use App\Models\JamPelajaran;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
